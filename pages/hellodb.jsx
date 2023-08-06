f20d21f7df9f1f94a052f68256beab2ed34d6e24
chinmay <mail2chinmaya.jena@gmail.com>
2023-08-06 22:06:49 +0530

    removed unncessary library

--- pages/hellodb.jsx
+++ pages/hellodb.jsx
@@ -1,23 +1,25 @@
 // pages/index.js
 import { connectDB } from "../lib/pgconnect";
-// import the react-json-view component
-import ReactJson from "react-json-view";
 
 export default function Home({ data }) {
-  // Use the database connection here
-
-  return <ReactJson src={data} />;
+  return (
+    <>
+      {data ? (
+        <span className="break-normal">{JSON.stringify(data)}</span>
+      ) : (
+        "Loading..."
+      )}
+    </>
+  );
 }
 
 export async function getServerSideProps() {
   const db = connectDB();
-
   try {
     const tableWithColumnMeta = [];
     const result = await db.query(`SELECT table_name 
     FROM information_schema.tables 
     WHERE table_schema='public' ;`);
-
     for (const table of result.rows) {
       let tableMeta = {};
       const tableMetadata = await db.query(
@@ -31,25 +33,20 @@ export async function getServerSideProps() {
       tableMeta["table_name"] = table.table_name;
       tableMeta["columns"] = [];
       tableMetadata.rows.forEach((row, index) => {
-        /*   console.log(
+        /*    console.log(
           `${index + 1}. Column: ${row.column_name}, Type: ${
             row.data_type
           }, Nullable: ${row.is_nullable}`
         ); */
-
         let colDetails = {};
         colDetails["col_name"] = row.column_name;
         colDetails["data_type"] = row.data_type;
         colDetails["is_nullable"] = row.is_nullable;
         tableMeta["columns"].push(colDetails);
       });
-
       tableWithColumnMeta.push(tableMeta);
     }
-
     const data = tableWithColumnMeta;
-    console.log(JSON.stringify(tableWithColumnMeta));
-
     return {
       props: { data },
     };
